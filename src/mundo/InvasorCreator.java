0b71ffc9af46b2794bbea51c298c98784cfb46c9
rengi0612 <rengi0612@gmail.com>
2023-04-14 14:56:37 -0500

    space

--- src/mundo/InvasorCreator.java
+++ src/mundo/InvasorCreator.java
@@ -4,5 +4,4 @@ public abstract class InvasorCreator {
     
     public abstract Enemigo createInvasor(double velocidad, int posX, int posY, int vida, int ancho, int alto, int direccion, String rutaImage, String ruta);
 
-    
 }
